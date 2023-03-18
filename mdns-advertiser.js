16ca1ba3aec64a3e53a9c2eafb3daf3479e14ce9
Brian Willy <midstarevolution@gmail.com>
2023-03-18 02:35:37 +0000

    convert port to int

--- mdns-advertiser.js
+++ mdns-advertiser.js
@@ -1,9 +1,15 @@
 // Example Avahi name: rpi-climate-sensor-prom-export-mylocation
+// To use this module in other projects, such as tempt-sensor
+// npm pack
+// cp mdns-module-1.0.0.tgz ../tempt-sensor/
+//
+// In order to run npm install on RPi for mdns/dnssd, this command will likely be required:
+// apt-get install libavahi-compat-libdnssd-dev
 
 const dnssd = require('dnssd');
 
 function startService(serviceName, serviceType, port) {
-    const service = new dnssd.Advertisement(dnssd.tcp(serviceType), port, {
+    const service = new dnssd.Advertisement(dnssd.tcp(serviceType), parseInt(port), {
         name: serviceName,
         txt: { version: '1.0.0' }
     });
